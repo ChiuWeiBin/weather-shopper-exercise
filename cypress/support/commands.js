937bdca6961f3b82e0f67955fc194b79a9942717
Wei Bin <chiuweibin.dev@gmail.com>
2020-06-14 14:29:01 +0800

    add custom command , code refactor

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -9,8 +9,43 @@
 // ***********************************************
 //
 //
+//import MoisturizersPage from "./PageObject/MoisturizersPage.spec";
+
+import MoisturizersPage from "../integration/PageObject/MoisturizersPage.spec";
+
+const moisturizersPage = new MoisturizersPage();
+
+Cypress.Commands.add("getminimumPrice", (includesWord1, includesWord2) => {
+  var minPrice1 = 99999;
+  moisturizersPage.getAllProductName().each((el, index, list) => {
+    const text = el.text();
+    if (text.includes(includesWord1) || text.includes(includesWord2)) {
+      moisturizersPage
+        .getAllProductName()
+        .eq(index)
+        .next()
+        .then(($price) => {
+          const priceInText = $price.text();
+          const currentPrice = priceInText.replace(/\D/g, "");
+          //cy.log(currentPrice);
+
+          if (currentPrice < minPrice1) {
+            minPrice1 = currentPrice;
+            //cy.log("minimum price  " + minPrice1);
+          }
+          //cy.log("minimum price 11 " + minPrice1);
+          cy.contains(minPrice1).next().as("AloeMinPriceBtn");
+          // cy.contains(minPrice1).next().click();
+          cy.contains(minPrice1).as("Price");
+        });
+      //cy.log("minimum price 1111 " + minPrice1);
+    }
+  });
+  cy.get("@AloeMinPriceBtn").click();
+});
+
 // -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
+
 //
 //
 // -- This is a child command --
